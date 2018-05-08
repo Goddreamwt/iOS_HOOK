a302b6397cc3db284686e16423bc231ebbe70f4f
Goddreamwt <zm804890716@163.com>
2018-05-08 18:03:41 +0800

    代理方法HOOK
    UIScrollView代理方法HOOK

---
+++ 008-加载网络图片/008-加载网络图片/UIScrollView+Intercepter.h
@@ -0,0 +1,13 @@
+//
+//  UIScrollView+Intercepter.h
+//  008-加载网络图片
+//
+//  Created by mac on 2018/5/8.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScrollView_Intercepter : UIScrollView
+
+@end
